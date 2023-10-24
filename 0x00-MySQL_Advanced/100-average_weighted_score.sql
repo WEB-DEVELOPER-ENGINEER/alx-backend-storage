243aef86c1b682a587d587fc42f9bd5b4b96cda2
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-24 11:03:50 -0700

    done

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,8 +6,8 @@ BEGIN
     UPDATE users AS U
     JOIN corrections AS C ON U.id = C.user_id
     JOIN projects AS P ON C.project_id = P.id
-    SET U.average_score = COALESCE(SUM(C.score * P.weight) / SUM(P.weight), 0)
     WHERE U.id = user_id;
+    SET U.average_score = COALESCE(SUM(C.score * P.weight) / SUM(P.weight), 0)
 END
 $$
 DELIMITER ;
