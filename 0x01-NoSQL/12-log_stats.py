a58979b3319b652113511b2f59f2ed7276751735
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-28 15:15:22 -0700

    done

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     """provides some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient()
+    client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     nginx_logs = nginx_collection.count_documents({})
     print(f'{nginx_logs} logs')
