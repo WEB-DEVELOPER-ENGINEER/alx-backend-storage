407a23362622551c136b774683f7eb00707246b4
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-28 15:22:31 -0700

    done

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,35 +3,31 @@
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def nginx_stats() -> Tuple:
     """provides some stats about Nginx logs stored in MongoDB"""
-    with MongoClient() as client:
-        db = client.logs
-        collection = db.nginx
-
-        methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-        method_stats = []
-
-        for method in methods:
-            method_count = collection.count_documents({'method': method})
-            method_stats.append({'method': method, 'count': method_count})
-
-        doc_count = collection.estimated_document_count()
-        status_path_stats = collection.count_documents({'method': 'GET', 'path': '/status'})
-
+    client = MongoClient()
+    collection = client.logs.nginx
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    method_stats = []
+    for method in methods:
+        method_count = collection.count_documents({'method': method})
+        method_stats.append({'method': method, 'count': method_count})
+    doc_count = collection.estimated_document_count()
+    status_path_stats = collection.count_documents({'method': 'GET',
+                                                    'path': '/status'})
+    client.close()
     return doc_count, method_stats, status_path_stats
 
 
 def print_nginx_stats() -> None:
     """
-    Prints stats from the nginx query.
+    Prints stats from nginx query
     """
-    doc_count, method_stats, status_path_stats = get_nginx_stats()
-
+    doc_count, method_stats, status_path_stats = nginx_stats()
     print(f'{doc_count} logs')
     print('Methods:')
     for method in method_stats:
-        print(f'\tmethod {method["method"]}: {method["count"]}')
+        print(f'\tmethod {method.get("method")}: {method.get("count")}')
     print(f'{status_path_stats} status check')
 
 
