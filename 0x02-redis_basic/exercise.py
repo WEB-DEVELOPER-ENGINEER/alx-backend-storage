ee58d4aa5285abeb78743a5a1d3268c2436a8f75
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-03 10:32:34 +0000

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -46,19 +46,17 @@ def call_history(method: Callable) -> Callable:
 
 
 def replay(method: Callable) -> None:
-    # sourcery skip: use-fstring-for-concatenation, use-fstring-for-formatting
     """
         display the history of calls of a particular function
     """
     name = method.__qualname__
     cache = redis.Redis()
     calls = cache.get(name).decode("utf-8")
-    print("{} was called {} times:".format(name, calls))
+    print(f"{name} was called {calls} times:")
     inputs = cache.lrange(name + ":inputs", 0, -1)
     outputs = cache.lrange(name + ":outputs", 0, -1)
     for i, o in zip(inputs, outputs):
-        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
-                                     o.decode('utf-8')))
+        print(f"{name}({i.decode('utf-8')}) -> {o.decode('utf-8')}")
 
 
 class Cache:
